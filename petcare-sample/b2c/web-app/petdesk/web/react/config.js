dc054b1f0f8c6251d755b1ac15acb6e798dfc4c1
badbish0p <141228440+badbish0p@users.noreply.github.com>
2024-05-03 15:56:15 +0530

    update configs

--- petcare-sample/b2c/web-app/petdesk/web/react/config.js
+++
@@ -1,9 +0,0 @@
-window.config = {
-    baseUrl: "",
-    clientID: "",
-    signInRedirectURL: "http://localhost:5173",
-    signOutRedirectURL: "http://localhost:5173",
-    myAccountAppURL: "",
-    resourceServerURL: "http://localhost:9090",
-    scope: ["openid", "email", "profile"]
- };
